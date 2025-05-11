f5bcdb51e9b4fd038f2ac37915dd35283a5580bd
Momeks <momeksdev@gmail.com>
2025-05-11 11:50:18 +0200

    Initial Commit

---
+++ TCA Networking/TCA_NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_NetworkingApp.swift
+//  TCA Networking
+//
+//  Created by Mohammad Komeili on 11.05.25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
